a1f0c1fd4139190969f00fc4b722ec452ae58cd6
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-05-02 17:56:59 -0400

    routes scafolding

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,12 +1,17 @@
 import express from 'express';
+import { currentUserRouter } from './routes/current-user';
+import { signInRouter } from './routes/sign-in';
+import { signOutRouter } from './routes/sign-out';
+import { signUpRouter } from './routes/sign-up';
 
 const app = express();
 
 app.use(express.json());
 
-app.get('/api/users/currentuser', (req, res) => {
-  res.send('Hi');
-});
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
 
 app.listen(3000, () => {
   console.log('🚀 ~ Auth Listening on port: 3000');
