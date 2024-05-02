2ca1d7faa112dd45029fd2dc19e92e9f905cb10b
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-05-02 18:41:23 -0400

    auth: sign up validations

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,9 +1,29 @@
 import express from 'express';
+import type { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 
 const router = express.Router();
 
-router.post('api/users/signup', (req, res) => {
-  res.send('signup');
+const validations = [
+  body('email').isEmail().withMessage('Email is not valid'),
+  body('password')
+    .trim()
+    .isLength({ min: 4, max: 20 })
+    .withMessage('Password must be between 4 and 20 characters'),
+];
+
+router.post('/api/users/signup', validations, (req: Request, res: Response) => {
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(400).send(errors.array());
+  }
+
+  const { email, password } = req.body;
+
+  console.log('creating user...');
+
+  res.send({});
 });
 
 export { router as signUpRouter };
