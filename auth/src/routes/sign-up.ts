142869a0640ca68885bba947e3d1a0ec6b4956c3
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-05-15 10:37:38 -0400

    feat: signup con jwt y cookie session

--- auth/src/routes/sign-up.ts
+++ auth/src/routes/sign-up.ts
@@ -1,8 +1,10 @@
 import express from 'express';
+import jwt from 'jsonwebtoken';
 import type { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
 import { ResquestValidationError } from '../errors/request-validation.error';
-import { DatabaseConnectionError } from '../errors/database-connection.error';
+import { User } from '../models/user';
+import { BadRequestError } from '../errors/bad-request.error';
 
 const router = express.Router();
 
@@ -14,19 +16,41 @@ const validations = [
     .withMessage('Password must be between 4 and 20 characters'),
 ];
 
-router.post('/api/users/signup', validations, (req: Request, res: Response) => {
-  const errors = validationResult(req);
+router.post(
+  '/api/users/signup',
+  validations,
+  async (req: Request, res: Response) => {
+    const errors = validationResult(req);
 
-  if (!errors.isEmpty()) {
-    throw new ResquestValidationError(errors.array());
-  }
+    if (!errors.isEmpty()) {
+      throw new ResquestValidationError(errors.array());
+    }
 
-  const { email, password } = req.body;
+    const { email, password } = req.body;
 
-  console.log('creating user...');
-  throw new DatabaseConnectionError();
+    const existingUser = await User.findOne({ email });
 
-  res.send({});
-});
+    if (existingUser) {
+      throw new BadRequestError('Email already in use.');
+    }
+
+    const newUser = User.build({ email, password });
+    await newUser.save();
+
+    // Generate JWT
+    const userJwt = jwt.sign(
+      {
+        id: newUser.id,
+        email: newUser.email,
+      },
+      process.env.JWT_KEY!,
+    );
+
+    // Store JWT on session
+    req.session = { jwt: userJwt };
+
+    return res.status(201).send(newUser);
+  },
+);
 
 export { router as signUpRouter };
