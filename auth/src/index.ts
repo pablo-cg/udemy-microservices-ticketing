077914fd8622f815109c96f9704d949e31e9f399
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-05-15 10:36:29 -0400

    feat: use cookie session

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -7,10 +7,20 @@ import { signUpRouter } from './routes/sign-up';
 import { errorHandler } from './middlewares/error-handler';
 import { NotFoundError } from './errors/not-found.error';
 import mongoose from 'mongoose';
+import cookieSession from 'cookie-session';
 
 const app = express();
 
+app.set('trust proxy', true);
+
 app.use(express.json());
+
+app.use(
+  cookieSession({
+    signed: false,
+    secure: true,
+  }),
+);
 
 app.use(currentUserRouter);
 app.use(signInRouter);
@@ -24,6 +34,10 @@ app.all('*', () => {
 app.use(errorHandler);
 
 async function start() {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('🚀 ~ start ~ connected to mongodb');
