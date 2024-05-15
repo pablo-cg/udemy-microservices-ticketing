504f2414a1076694ced98cae645b700277eeea94
Pablo Cuevas <cuevasgpablo@gmail.com>
2024-05-15 10:37:05 -0400

    transform user

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -15,16 +15,28 @@ interface UserModel extends Model<UserDocument> {
   build(attrs: UserAttrs): UserDocument;
 }
 
-const userSchema = new Schema({
-  email: {
-    type: String,
-    required: true,
+const userSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
   },
-  password: {
-    type: String,
-    required: true,
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
   },
-});
+);
 
 userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
